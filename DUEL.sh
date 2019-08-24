86d4dbe85ce6b5d86eb2f4bb267ed3edb5fde9f0
greenzombies123 <52916652+greenzombies123@users.noreply.github.com>
2019-08-24 10:15:51 +0700

    Add files via upload

---
+++ DUEL.sh
@@ -0,0 +1,243 @@
+blue='\033[34;1m'
+green='\033[32;1m'  
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+clear
+echo 
+echo
+echo $red"  ▄████  ██▀███  ▓█████ ▓█████  ███▄    █ "
+sleep 1
+echo $red" ██▒ ▀█▒▓██ ▒ ██▒▓█   ▀ ▓█   ▀  ██ ▀█   █ "
+sleep 1
+echo $red"▒██░▄▄▄░▓██ ░▄█ ▒▒███   ▒███   ▓██  ▀█ ██▒"
+echo $red"░▓█  ██▓▒██▀▀█▄  ▒▓█  ▄ ▒▓█  ▄ ▓██▒  ▐▌██▒"
+sleep 1
+echo $red"░▒▓███▀▒░██▓ ▒██▒░▒████▒░▒████▒▒██░   ▓██░"
+echo $red" ░▒   ▒ ░ ▒▓ ░▒▓░░░ ▒░ ░░░ ▒░ ░░ ▒░   ▒ ▒ "
+echo $red"  ░   ░   ░▒ ░ ▒░ ░ ░  ░ ░ ░  ░░ ░░   ░ ▒░"
+sleep 1
+echo $red"░ ░   ░   ░░   ░    ░      ░      ░   ░ ░ "
+sleep 1
+echo $red"      ░    ░        ░  ░   ░  ░         ░ "
+echo
+echo
+echo $white"▒███████▒ ▒█████   ███▄ ▄███▓ ▄▄▄▄    ██▓▓█████   ██████ "
+sleep 1
+echo $white"▒ ▒ ▒ ▄▀░▒██▒  ██▒▓██▒▀█▀ ██▒▓█████▄ ▓██▒▓█   ▀ ▒██    ▒ "
+sleep 1
+echo $white"░ ▒ ▄▀▒░ ▒██░  ██▒▓██    ▓██░▒██▒ ▄██▒██▒▒███   ░ ▓██▄   "
+echo $white"  ▄▀▒   ░▒██   ██░▒██    ▒██ ▒██░█▀  ░██░▒▓█  ▄   ▒   ██▒"
+sleep 1
+echo $white"▒███████▒░ ████▓▒░▒██▒   ░██▒░▓█  ▀█▓░██░░▒████▒▒██████▒▒"
+echo $white"░▒▒ ▓░▒░▒░ ▒░▒░▒░ ░ ▒░   ░  ░░▒▓███▀▒░▓  ░░ ▒░ ░▒ ▒▓▒ ▒ ░"
+sleep 1
+echo $white"░░▒ ▒ ░ ▒  ░ ▒ ▒░ ░  ░      ░▒░▒   ░  ▒ ░ ░ ░  ░░ ░▒  ░ ░"
+sleep 1
+echo $white"░ ░ ░ ░ ░░ ░ ░ ▒  ░      ░    ░    ░  ▒ ░   ░   ░  ░  ░  "
+echo $white"  ░ ░        ░ ░         ░    ░       ░     ░  ░      ░  "
+echo $white"░                                  ░               "
+echo $red"______________________________________________"
+echo $red" [+]Author by    : GREEN ZOMBIES "
+sleep 1
+echo $red" [+]Team           : AGEN SEA         "
+sleep 1
+echo $red" [+]Thanks to      : MR_81"
+echo $red" [+]Thanks to      : MR_ARIANIMOUS"
+echo $white" [+]Thanks to      : MR_XZ.6"
+sleep 1
+echo $white" [+]Thanks to      : B3kT0509"
+echo $white" [+]Thanks to      : MR_VIRUS"
+sleep 1
+echo $white" [+]Thanks to      : MR_DENIMNEWBIE"
+echo $white" [+]Thanks to      :꧁࿗ｲu4刀乙•ｱ尺o√oズﾑｲo尺࿗꧂"
+sleep 1
+echo $white"__________________________________________"
+figlet -f smshadow "kalau"
+figlet -f smshadow "takut"
+figlet -f smshadow "tidur"
+figlet -f smshadow "aja"
+echo $white"_________________________________________"
+sleep 1
+echo $white"[1]"$green"DUEL DDOS"
+echo $white"________________________________________"
+sleep 1
+echo $white"[2]"$green"DUEL DEFACE TEBAS INDEX"
+echo $white"________________________________________"
+sleep 1
+echo $white"[3]"$green"DUEL TROJAN HORSE [rat]"
+echo $white"________________________________________"
+echo "\033[34;1m  99\033[37;1m.\033[32;1mKeluar\033[37;1m"
+echo ""
+echo "\033[32;1m ──> \033[32;1m PILIH DENGAN BIJAK!\033[32;1m"
+read -p " GREENZOMBIES$ " pilih
+if [ $pilih = 1 ]
+then
+clear
+echo $red"  ▄████  ██▀███  ▓█████ ▓█████  ███▄    █ "
+sleep 1
+echo $red" ██▒ ▀█▒▓██ ▒ ██▒▓█   ▀ ▓█   ▀  ██ ▀█   █ "
+echo $red"▒██░▄▄▄░▓██ ░▄█ ▒▒███   ▒███   ▓██  ▀█ ██▒"
+echo $red"░▓█  ██▓▒██▀▀█▄  ▒▓█  ▄ ▒▓█  ▄ ▓██▒  ▐▌██▒"
+echo $red"░▒▓███▀▒░██▓ ▒██▒░▒████▒░▒████▒▒██░   ▓██░"
+sleep 1
+echo $red" ░▒   ▒ ░ ▒▓ ░▒▓░░░ ▒░ ░░░ ▒░ ░░ ▒░   ▒ ▒ "
+echo $red"  ░   ░   ░▒ ░ ▒░ ░ ░  ░ ░ ░  ░░ ░░   ░ ▒░"
+echo $red"░ ░   ░   ░░   ░    ░      ░      ░   ░ ░ "
+sleep 1
+echo $red"      ░    ░        ░  ░   ░  ░         ░ "
+echo
+echo
+echo $white"▒███████▒ ▒█████   ███▄ ▄███▓ ▄▄▄▄    ██▓▓█████   ██████ "
+sleep 1
+echo $white"▒ ▒ ▒ ▄▀░▒██▒  ██▒▓██▒▀█▀ ██▒▓█████▄ ▓██▒▓█   ▀ ▒██    ▒ "
+sleep 1
+echo $white"░ ▒ ▄▀▒░ ▒██░  ██▒▓██    ▓██░▒██▒ ▄██▒██▒▒███   ░ ▓██▄   "
+echo $white"  ▄▀▒   ░▒██   ██░▒██    ▒██ ▒██░█▀  ░██░▒▓█  ▄   ▒   ██▒"
+sleep 1
+echo $white"▒███████▒░ ████▓▒░▒██▒   ░██▒░▓█  ▀█▓░██░░▒████▒▒██████▒▒"
+echo $white"░▒▒ ▓░▒░▒░ ▒░▒░▒░ ░ ▒░   ░  ░░▒▓███▀▒░▓  ░░ ▒░ ░▒ ▒▓▒ ▒ ░"
+sleep 1
+echo $white"░░▒ ▒ ░ ▒  ░ ▒ ▒░ ░  ░      ░▒░▒   ░  ▒ ░ ░ ░  ░░ ░▒  ░ ░"
+sleep 1
+echo $white"░ ░ ░ ░ ░░ ░ ░ ▒  ░      ░    ░    ░  ▒ ░   ░   ░  ░  ░  "
+echo $white"  ░ ░        ░ ░         ░    ░       ░     ░  ░      ░  "
+echo $white"░                                  ░               "
+echo $red"______________________________________________"
+echo $red" [+]Author by    : GREEN ZOMBIES "
+sleep 1
+echo $red" [+]Team           : AGEN SEA         "
+sleep 1
+echo $red" [+]Thanks to      : MR_81"
+echo $red" [+]Thanks to      : MR_ARIANIMOUS"
+echo $white" [+]Thanks to      : MR_XZ.6"
+sleep 1
+echo $white" [+]Thanks to      : B3kT0509"
+echo $white" [+]Thanks to      : MR_VIRUS"
+sleep 1
+echo $white" [+]Thanks to      : MR_DENIMNEWBIE"
+echo $white" [+]Thanks to      :꧁࿗ｲu4刀乙•ｱ尺o√oズﾑｲo尺࿗꧂"
+sleep 1
+echo $white"__________________________________________"
+echo "\033[34;1mLoading.....!!!"
+echo
+sleep 1
+apt update && apt upgrade
+pkg install git
+pkg install bash
+git clone https://github.com/greenzombies123/duel1
+ls
+cd duel1
+sh duel1.sh
+fi
+
+if [ $pilih = 2 ]
+then
+clear
+echo $red"  ▄████  ██▀███  ▓█████ ▓█████  ███▄    █ "
+sleep 1
+echo $red" ██▒ ▀█▒▓██ ▒ ██▒▓█   ▀ ▓█   ▀  ██ ▀█   █ "
+echo $red"▒██░▄▄▄░▓██ ░▄█ ▒▒███   ▒███   ▓██  ▀█ ██▒"
+echo $red"░▒▓███▀▒░██▓ ▒██▒░▒████▒░▒████▒▒██░   ▓██░"
+echo $red"  ░   ░   ░▒ ░ ▒░ ░ ░  ░ ░ ░  ░░ ░░   ░ ▒░"
+sleep 1
+echo $red"      ░    ░        ░  ░   ░  ░         ░ "
+echo
+echo
+echo $white"▒███████▒ ▒█████   ███▄ ▄███▓ ▄▄▄▄    ██▓▓█████   ██████ "
+sleep 1
+echo $white"▒ ▒ ▒ ▄▀░▒██▒  ██▒▓██▒▀█▀ ██▒▓█████▄ ▓██▒▓█   ▀ ▒██    ▒ "
+sleep 1
+echo $white"░ ▒ ▄▀▒░ ▒██░  ██▒▓██    ▓██░▒██▒ ▄██▒██▒▒███   ░ ▓██▄   "
+echo $white"  ▄▀▒   ░▒██   ██░▒██    ▒██ ▒██░█▀  ░██░▒▓█  ▄   ▒   ██▒"
+sleep 1
+echo $white"▒███████▒░ ████▓▒░▒██▒   ░██▒░▓█  ▀█▓░██░░▒████▒▒██████▒▒"
+echo $white"░▒▒ ▓░▒░▒░ ▒░▒░▒░ ░ ▒░   ░  ░░▒▓███▀▒░▓  ░░ ▒░ ░▒ ▒▓▒ ▒ ░"
+sleep 1
+echo $white"░░▒ ▒ ░ ▒  ░ ▒ ▒░ ░  ░      ░▒░▒   ░  ▒ ░ ░ ░  ░░ ░▒  ░ ░"
+sleep 1
+echo $white"░ ░ ░ ░ ░░ ░ ░ ▒  ░      ░    ░    ░  ▒ ░   ░   ░  ░  ░  "
+echo $white"  ░ ░        ░ ░         ░    ░       ░     ░  ░      ░  "
+echo $white"░                                  ░               "
+echo $red"______________________________________________"
+echo $red" [+]Author by    : GREEN ZOMBIES "
+sleep 1
+echo $red" [+]Team           : AGEN SEA         "
+sleep 1
+echo $red" [+]Thanks to      : MR_81"
+echo $red" [+]Thanks to      : MR_ARIANIMOUS"
+echo $white" [+]Thanks to      : MR_XZ.6"
+sleep 1
+echo $white" [+]Thanks to      : B3kT0509"
+echo $white" [+]Thanks to      : MR_VIRUS"
+sleep 1
+echo $white" [+]Thanks to      : MR_DENIMNEWBIE"
+echo $white" [+]Thanks to      :꧁࿗ｲu4刀乙•ｱ尺o√oズﾑｲo尺࿗꧂"
+sleep 1
+echo $white"__________________________________________"
+sleep 1
+apt update && apt upgrade
+pkg install git
+pkg install bash
+git clone https://github.com/greenzombies123/duel1
+ls
+cd duel1
+sh duel1.sh
+fi
+
+if [ $pilih = 3 ]
+then
+clear
+echo $red"  ▄████  ██▀███  ▓█████ ▓█████  ███▄    █ "
+sleep 1
+echo $red" ██▒ ▀█▒▓██ ▒ ██▒▓█   ▀ ▓█   ▀  ██ ▀█   █ "
+ echo $red"▒██░▄▄▄░▓██ ░▄█ ▒▒███   ▒███   ▓██  ▀█ ██▒"
+echo $red"░▒▓███▀▒░██▓ ▒██▒░▒████▒░▒████▒▒██░   ▓██░"
+echo $red"  ░   ░   ░▒ ░ ▒░ ░ ░  ░ ░ ░  ░░ ░░   ░ ▒░"
+sleep 1
+echo $red"      ░    ░        ░  ░   ░  ░         ░ "
+echo
+echo
+echo $white"▒███████▒ ▒█████   ███▄ ▄███▓ ▄▄▄▄    ██▓▓█████   ██████ "
+sleep 1
+echo $white"▒ ▒ ▒ ▄▀░▒██▒  ██▒▓██▒▀█▀ ██▒▓█████▄ ▓██▒▓█   ▀ ▒██    ▒ "
+sleep 1
+echo $white"░ ▒ ▄▀▒░ ▒██░  ██▒▓██    ▓██░▒██▒ ▄██▒██▒▒███   ░ ▓██▄   "
+echo $white"  ▄▀▒   ░▒██   ██░▒██    ▒██ ▒██░█▀  ░██░▒▓█  ▄   ▒   ██▒"
+sleep 1
+echo $white"▒███████▒░ ████▓▒░▒██▒   ░██▒░▓█  ▀█▓░██░░▒████▒▒██████▒▒"
+echo $white"░▒▒ ▓░▒░▒░ ▒░▒░▒░ ░ ▒░   ░  ░░▒▓███▀▒░▓  ░░ ▒░ ░▒ ▒▓▒ ▒ ░"
+sleep 1
+echo $white"░░▒ ▒ ░ ▒  ░ ▒ ▒░ ░  ░      ░▒░▒   ░  ▒ ░ ░ ░  ░░ ░▒  ░ ░"
+sleep 1
+echo $white"░ ░ ░ ░ ░░ ░ ░ ▒  ░      ░    ░    ░  ▒ ░   ░   ░  ░  ░  "
+echo $white"  ░ ░        ░ ░         ░    ░       ░     ░  ░      ░  "
+echo $white"░                                  ░               "
+echo $red"______________________________________________"
+echo $red" [+]Author by    : GREEN ZOMBIES "
+sleep 1
+echo $red" [+]Team           : AGEN SEA         "
+sleep 1
+echo $red" [+]Thanks to      : MR_81"
+echo $red" [+]Thanks to      : MR_ARIANIMOUS"
+echo $white" [+]Thanks to      : MR_XZ.6"
+sleep 1
+echo $white" [+]Thanks to      : B3kT0509"
+echo $white" [+]Thanks to      : MR_VIRUS"
+sleep 1
+echo $white" [+]Thanks to      : MR_DENIMNEWBIE"
+echo $white" [+]Thanks to      :꧁࿗ｲu4刀乙•ｱ尺o√oズﾑｲo尺࿗꧂"
+echo $white"__________________________________________"
+sleep 1
+apt update && apt upgrade
+pkg install git
+pkg install bash
+git clone https://github.com/greenzombies123/duel1
+ls
+cd duel1
+sh duel1.sh
+fi
+
+if
